37b502a0fcbad01ee495a62d2a7351a1e614d69c
AlenKljuno <80717857+AlenKljuno@users.noreply.github.com>
2021-04-30 17:16:17 +0200

    Add files via upload

---
+++ html page/script.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#app',
+    data () {
+      return {
+        showModal : false,
+        showModal2 : false,
+        showModal3 : false,
+        showModal4 : false,
+        showModal5 : false,
+        modals : [{title:'S%RCLOTHING',desc:''}],
+        modals2 : [ {title:'Shoes',desc:'A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion. The design of shoes has varied enormously through time and from culture to culture, with appearance originally being tied to function.Price for these shoes is 90$'}],
+        modals3 : [ {title:'Shirt',desc:'A shirt is a cloth garment for the upper body (from the neck to the waist). Originally an undergarment worn exclusively by men, it has become, in American English, a catch-all term for a broad variety of upper-body garments and undergarments. ... A shirt can also be worn with a necktie under the shirt collar.'}],
+        modals4 : [ {title:'HandBag',desc:'A bag is a kind of soft container. It can hold or carry things. It may be made from cloth, leather, plastic, or paper. Many bags are disposable but some are made to use for a long time. A bag may have one or two handles or a shoulder strap. Bags come in different shapes and sizes depending on how they will be used. A small bag that can be carried with a single hand is sometimes called a handbag, purse, or pocketbook. Children can suffocate, or stop breathing and die, when they put certain kinds of bags over their heads, due to having the supply of oxygen cut off.'}]
+      };  
+    }
+        
+    
+    
+    
+})
